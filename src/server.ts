b08f7f9643548d3003ece52197d87ed086e8cd3a
Patryk Peszko <me@patrykpeszko.com>
2018-07-20 19:02:51 +0200

    Read and watch config file

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,56 @@
 /* tslint:disable:no-console */
 import commander from 'commander';
+import fs from 'fs';
+import path from 'path';
 import app from './app';
 
 commander.option('-c, --config <path>', 'Path to config file').parse(process.argv);
 
-let { config } = commander;
-if (config) {
-  console.log('Reading config file from: ' + config);
-} else {
-  config = './apimocker.json';
+class Server {
+  port = process.env.PORT || 3000;
+
+  readFile = (configPath: string) => {
+    console.log('Reading: ' + configPath);
+    fs.readFile(configPath, { encoding: 'utf-8' }, (error, data) => {
+      if (!error) {
+        console.log('Received: ' + data);
+        const json = JSON.parse(data);
+        console.log('json test: ' + json.test);
+      } else {
+        console.error(error);
+      }
+    });
+  };
+
+  watchConfigForChanges = (configPath: string) => {
+    fs.watchFile(configPath, () => {
+      console.log(configPath + ' changed');
+      this.readFile(configPath);
+    });
+  };
+
+  run = () => {
+    console.log('Starting run');
+    let { config } = commander;
+    if (config) {
+      console.log('Reading config file from: ' + config);
+    } else {
+      config = './apimocker.json';
+    }
+    console.log(config);
+    const filePath = path.join(__dirname, '..', config);
+    this.readFile(filePath);
+    this.watchConfigForChanges(filePath);
+
+    app.listen(this.port, (err: any) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      return console.log(`server is listening on ${this.port}`);
+    });
+  };
 }
-console.log(commander.config);
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, (err: any) => {
-  if (err) {
-    return console.log(err);
-  }
-
-  return console.log(`server is listening on ${port}`);
-});
+const server = new Server();
+server.run();
