8b9111113d9255b5a613121bb5f79a7df8b683ae
Patryk Peszko <me@patrykpeszko.com>
2018-08-06 21:42:39 +0200

    Remove some some noisy output

--- src/server.ts
+++ src/server.ts
@@ -12,10 +12,8 @@ class Server {
   port = process.env.PORT || 3000;
 
   readFile = (configPath: string) => {
-    console.log('Reading: ' + configPath);
     fs.readFile(configPath, { encoding: 'utf-8' }, (error, data) => {
       if (!error) {
-        console.log('Received: ' + data);
         const json = JSON.parse(data);
         const config = this.parseConfig(json);
       } else {
@@ -34,20 +32,16 @@ class Server {
 
   parseConfig = (configData: any): IConfig => {
     const normalizedData = normalize(configData, ConfigSchema);
-    console.log('-------------------------------');
-    console.log(normalizedData);
     return {} as IConfig;
   };
 
   run = () => {
     console.log('Starting run');
     let { configData } = commander;
-    if (configData) {
-      console.log('Reading config file from: ' + configData);
-    } else {
+    if (!configData) {
       configData = './apimocker.json';
     }
-    console.log(configData);
+    console.log('Reading config file from: ' + configData);
     this.readFile(configData);
     this.watchConfigForChanges(configData);
 
@@ -55,7 +49,7 @@ class Server {
     const app = new App(configData);
     app.express.listen(this.port, (err: any) => {
       if (err) {
-        return console.log(err);
+        return console.error(err);
       }
 
       return console.log(`server is listening on ${this.port}`);
