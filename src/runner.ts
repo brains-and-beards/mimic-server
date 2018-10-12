57cea8d5c40180fbd4a4833cdcb4429208b269de
Marek Waligórski <marek@brainsandbeards.com>
2018-10-12 12:09:54 +0200

    Add option to show server version

--- src/runner.ts
+++ src/runner.ts
@@ -3,7 +3,10 @@ import commander from 'commander';
 
 import Server from './server';
 
-commander.option('-c, --config <path>', 'Path to config file').parse(process.argv);
+commander
+  .version('0.0.1', '-v, --version')
+  .option('-c, --config <path>', 'Path to config file')
+  .parse(process.argv);
 
 const { config } = commander;
 
