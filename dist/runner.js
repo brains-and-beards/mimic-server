4c86fe43006163da299f616f4e743f2fa43e3026
Marek Waligórski <marek@brainsandbeards.com>
2018-10-12 12:44:36 +0200

    Udpate dist/

--- dist/runner.js
+++ dist/runner.js
@@ -6,7 +6,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = __importDefault(require("commander"));
 const server_1 = __importDefault(require("./server"));
-commander_1.default.option('-c, --config <path>', 'Path to config file').parse(process.argv);
+const package_json_1 = __importDefault(require("../package.json"));
+commander_1.default
+    .version(package_json_1.default.version, '-v, --version')
+    .option('-c, --config <path>', 'Path to config file')
+    .parse(process.argv);
 const { config } = commander_1.default;
 const server = new server_1.default(config);
 server.run();
