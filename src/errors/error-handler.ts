fd547dc568feeda7920a59dc815a212504911cf0
Marek Waligórski <marek@brainsandbeards.com>
2018-10-26 14:44:27 +0200

    Allow to pass a custom error handler that does not exit the app

--- src/errors/error-handler.ts
+++ src/errors/error-handler.ts
@@ -1,20 +1,28 @@
-export class ErrorHandler {
-  static checkErrorAndStopProcess(error: any) {
+class ErrorHandler {
+  onError: (code?: number) => void;
+
+  constructor(onError?: (code?: number) => void) {
+    this.onError = onError || ((code?: number) => process.exit(code));
+  }
+
+  checkErrorAndStopProcess = (error: any) => {
+    let code;
+
     if (error.code && error.code === 'ENOENT' && error.path && error.path.indexOf('apimocker.json')) {
       console.error('Error - code 20 - config file is not present');
-      process.exit(20);
+      code = 20;
     }
     if (error instanceof SyntaxError) {
       console.error('Error - code 21 - error during config file parsing');
-      process.exit(21);
+      code = 21;
     }
     if (error.code && error.code === 'EADDRINUSE' && error.port) {
       console.error('Error - code 22 - Server address/port is already used by another process - port = ' + error.port);
-      process.exit(22);
+      code = 22;
     }
 
-    // unhandled error - show raw error message and exit
-    console.error(error);
-    process.exit();
-  }
+    this.onError(code);
+  };
 }
+
+export default ErrorHandler;
