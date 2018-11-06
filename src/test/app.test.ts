4381ea6ed18d6a08087f85a24c20749e743738e9
Natalia Majkowska <natalia@brainsandbeards.com>
2018-11-06 10:41:18 +0100

    Add test for server

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -1,26 +1,41 @@
 import supertest = require('supertest');
+// import XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-describe('http', () => {
-  it('http-test', () => {
-    const request = supertest('http://localhost:3000');
+// describe('http', () => {
+//   it('http-test', () => {
+//     const request = supertest('http://localhost:3000');
 
-    const xhr = new XMLHttpRequest();
+//     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', url, true);
-    xhr.send();
+//     xhr.open('GET', url, true);
+//     xhr.send();
 
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        console.log(xhr.responseText);
-      }
-    };
+//     xhr.onreadystatechange = () => {
+//       if (xhr.readyState === 4) {
+//         console.log(xhr.responseText);
+//       }
+//     };
 
-    request
-      .get('/project/test')
-      .expect({ message: 'this is the result' })
-      .expect(200, (err, res) => {
-        console.log(err);
-        console.log(res.body);
-      });
+//     request
+//       .get('/project/test')
+//       .expect({ message: 'this is the result' })
+//       .expect(200, (err, res) => {
+//         console.log(err);
+//         console.log(res.body);
+//       });
+//   });
+// });
+
+import request from 'supertest-as-promised';
+import Server from '../server';
+
+describe('Flow API', () => {
+  it('hello test', async () => {
+    const res = await request('http://localhost:3000/nowy')
+      .get('/a')
+      .expect(200);
+    // console.log('TCL: res', res);
+    expect(typeof res.body).toBe('object');
+    expect(res.body.single).toBe('forever alone');
   });
 });
