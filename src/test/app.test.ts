c8ac3e511137350df645e84d547ec0b13d4ba84a
Natalia Majkowska <natalia@brainsandbeards.com>
2018-11-07 21:27:06 +0100

    Refactor tests and add test for post

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -1,41 +1,29 @@
-import supertest = require('supertest');
-// import XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-// describe('http', () => {
-//   it('http-test', () => {
-//     const request = supertest('http://localhost:3000');
-
-//     const xhr = new XMLHttpRequest();
-
-//     xhr.open('GET', url, true);
-//     xhr.send();
-
-//     xhr.onreadystatechange = () => {
-//       if (xhr.readyState === 4) {
-//         console.log(xhr.responseText);
-//       }
-//     };
-
-//     request
-//       .get('/project/test')
-//       .expect({ message: 'this is the result' })
-//       .expect(200, (err, res) => {
-//         console.log(err);
-//         console.log(res.body);
-//       });
-//   });
-// });
-
 import request from 'supertest-as-promised';
-import Server from '../server';
 
-describe('Flow API', () => {
-  it('hello test', async () => {
-    const res = await request('http://localhost:3000/nowy')
-      .get('/a')
+describe('Tests for testmocker.json', () => {
+  it('[GET] Should return 200 for empty body', async () => {
+    const res = await request('http://localhost:3000/test')
+      .get('/abc')
       .expect(200);
-    // console.log('TCL: res', res);
     expect(typeof res.body).toBe('object');
-    expect(res.body.single).toBe('forever alone');
+    expect(res.body).toEqual({});
   });
+
+  // TODO: remove comment after merge body validation
+  // it('[POST] Should return 200 for empty body', async () => {
+  //   const res = await request('http://localhost:3000/test')
+  //     .post('/abc')
+  //     .send({ param1: 'param1' })
+  //     .expect(200);
+  //   expect(typeof res.body).toBe('object');
+  //   expect(res.body.response).toEqual('response');
+  // });
+
+  // it('[POST] Should return 200 and string response for empty body', async () => {
+  //   const res = await request('http://localhost:3000/test')
+  //     .post('/abc')
+  //     .expect(200);
+  //   expect(typeof res.body).toBe('string');
+  //   expect(res.body).toEqual('response');
+  // });
 });
