961b2cea3781d4ca61377046ae1aeb4a81c050bd
Giorgio Provenzale <giorgio.provenzale@gmail.com>
2018-11-08 16:15:39 +0100

    General project tests added

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest-as-promised';
 
 const projectBasePath = 'http://localhost:3000/test';
+const projectWithoutEndpointsBasePath = 'http://localhost:3000/test_no_endpoints';
+const basePathWithoutProjectValue = 'http://localhost:3000/';
 
 const contentTypeJSON = 'application/json; charset=utf-8';
 const contentTypeText = 'text/html; charset=utf-8';
@@ -61,7 +63,7 @@ describe('Tests for testmocker.json - GET', () => {
     expect(typeof res.body).toBe('object');
     expect(res.body).toEqual({});
   });
-  it('[GET - params] - Should return 200 with object as body with specific string value', async () => {
+  it('[GET - params] - Should return 200 with JSON response with specific string value', async () => {
     const res = await request(projectBasePath)
       .get('/object?param=value')
       .expect(200)
@@ -70,7 +72,7 @@ describe('Tests for testmocker.json - GET', () => {
     expect(typeof res.body.hey).toBe('string');
     expect(res.body.hey).toBe('I am working with params');
   });
-  it('[GET - params] - Should return 200 with object as body with specific number value', async () => {
+  it('[GET - params] - Should return 200 with JSON response with specific number value', async () => {
     const res = await request(projectBasePath)
       .get('/object?param=value')
       .expect(200)
@@ -79,7 +81,7 @@ describe('Tests for testmocker.json - GET', () => {
     expect(typeof res.body.num).toBe('number');
     expect(res.body.num).toBe(8);
   });
-  it('[GET - params] - Should return 200 with object as body with specific boolean value', async () => {
+  it('[GET - params] - Should return 200 with JSON response with specific boolean value', async () => {
     const res = await request(projectBasePath)
       .get('/object?param=value')
       .expect(200)
@@ -88,6 +90,11 @@ describe('Tests for testmocker.json - GET', () => {
     expect(typeof res.body.valid).toBe('boolean');
     expect(res.body.valid).toBe(false);
   });
+  it('[GET - params] - Should return 404 with wrong parameters - JSON response', async () => {
+    const res = await request(projectBasePath)
+      .get('/object?parameter=value_is_not_correct')
+      .expect(404);
+  });
   it('[GET - params] - Should return 200 with raw response with specific string', async () => {
     const res = await request(projectBasePath)
       .get('/string?param=value')
@@ -95,9 +102,24 @@ describe('Tests for testmocker.json - GET', () => {
       .expect('Content-Type', contentTypeText);
     expect(res.text).toBe('response with params');
   });
+  it('[GET - params] - Should return 404 with wrong parameters - raw response', async () => {
+    const res = await request(projectBasePath)
+      .get('/string?parameter=value_is_not_correct')
+      .expect(404);
+  });
   it('[GET - params] - Should return 404', async () => {
     const res = await request(projectBasePath)
       .get('/unknown?param=value')
+      .expect(404);
+  });
+  it('[GET - no project value] - Should return 404', async () => {
+    const res = await request(basePathWithoutProjectValue)
+      .get('/endpoint')
+      .expect(404);
+  });
+  it('[GET - project without endpoints] - Should return 404', async () => {
+    const res = await request(projectWithoutEndpointsBasePath)
+      .get('/endpoint')
       .expect(404);
   });
 
