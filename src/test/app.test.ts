6f80f882f3748095d741b0b3517c67901416913f
Giorgio Provenzale <giorgio.provenzale@gmail.com>
2018-11-08 15:16:40 +0100

    GET tests

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -2,38 +2,103 @@ import request from 'supertest-as-promised';
 
 const projectBasePath = 'http://localhost:3000/test';
 
-describe('Tests for testmocker.json', () => {
+const contentTypeJSON = 'application/json; charset=utf-8';
+const contentTypeText = 'text/html; charset=utf-8';
+
+describe('Tests for testmocker.json - GET', () => {
   it('[GET] - Should return 200 for empty body', async () => {
     const res = await request(projectBasePath)
-      .get('/abc')
-      .expect(200);
+      .get('/empty')
+      .expect(200)
+      .expect('Content-Type', contentTypeJSON);
     expect(typeof res.body).toBe('object');
     expect(res.body).toEqual({});
   });
-  it('[GET] - Should return 200 with body', async () => {
+  it('[GET] - Should return 200 with object as body with specific string value', async () => {
     const res = await request(projectBasePath)
       .get('/object')
-      .expect(200);
-  });
-  it('[GET] - Should return object as body', async () => {
-    const res = await request(projectBasePath).get('/object');
+      .expect(200)
+      .expect('Content-Type', contentTypeJSON);
     expect(typeof res.body).toBe('object');
-  });
-  it('[GET] - Should return a specific value', async () => {
-    const res = await request(projectBasePath).get('/object');
+    expect(typeof res.body.hey).toBe('string');
     expect(res.body.hey).toBe('I am working');
   });
-  it('[GET] - Should return a string', async () => {
-    const res = await request(projectBasePath).get('/object');
-    expect(typeof res.body.hey).toBe('string');
-  });
-  it('[GET] - Should return a number', async () => {
-    const res = await request(projectBasePath).get('/object');
+  it('[GET] - Should return 200 with object as body with specific number value', async () => {
+    const res = await request(projectBasePath)
+      .get('/object')
+      .expect(200)
+      .expect('Content-Type', contentTypeJSON);
+    expect(typeof res.body).toBe('object');
     expect(typeof res.body.num).toBe('number');
+    expect(res.body.num).toBe(4);
   });
-  it('[GET] - Should return a specific value with params', async () => {
-    const res = await request(projectBasePath).get('/object?param=value');
+  it('[GET] - Should return 200 with object as body with specific boolean value', async () => {
+    const res = await request(projectBasePath)
+      .get('/object')
+      .expect(200)
+      .expect('Content-Type', contentTypeJSON);
+    expect(typeof res.body).toBe('object');
+    expect(typeof res.body.valid).toBe('boolean');
+    expect(res.body.valid).toBe(true);
+  });
+  it('[GET] - Should return 200 with raw response with specific string', async () => {
+    const res = await request(projectBasePath)
+      .get('/string')
+      .expect(200)
+      .expect('Content-Type', contentTypeText);
+    expect(res.text).toBe('response');
+  });
+  it('[GET] - Should return 404', async () => {
+    const res = await request(projectBasePath)
+      .get('/unknown')
+      .expect(404);
+  });
+  it('[GET - params] - Should return 200 for empty body', async () => {
+    const res = await request(projectBasePath)
+      .get('/empty?param=value')
+      .expect(200)
+      .expect('Content-Type', contentTypeJSON);
+    expect(typeof res.body).toBe('object');
+    expect(res.body).toEqual({});
+  });
+  it('[GET - params] - Should return 200 with object as body with specific string value', async () => {
+    const res = await request(projectBasePath)
+      .get('/object?param=value')
+      .expect(200)
+      .expect('Content-Type', contentTypeJSON);
+    expect(typeof res.body).toBe('object');
+    expect(typeof res.body.hey).toBe('string');
     expect(res.body.hey).toBe('I am working with params');
+  });
+  it('[GET - params] - Should return 200 with object as body with specific number value', async () => {
+    const res = await request(projectBasePath)
+      .get('/object?param=value')
+      .expect(200)
+      .expect('Content-Type', contentTypeJSON);
+    expect(typeof res.body).toBe('object');
+    expect(typeof res.body.num).toBe('number');
+    expect(res.body.num).toBe(8);
+  });
+  it('[GET - params] - Should return 200 with object as body with specific boolean value', async () => {
+    const res = await request(projectBasePath)
+      .get('/object?param=value')
+      .expect(200)
+      .expect('Content-Type', contentTypeJSON);
+    expect(typeof res.body).toBe('object');
+    expect(typeof res.body.valid).toBe('boolean');
+    expect(res.body.valid).toBe(false);
+  });
+  it('[GET - params] - Should return 200 with raw response with specific string', async () => {
+    const res = await request(projectBasePath)
+      .get('/string?param=value')
+      .expect(200)
+      .expect('Content-Type', contentTypeText);
+    expect(res.text).toBe('response with params');
+  });
+  it('[GET - params] - Should return 404', async () => {
+    const res = await request(projectBasePath)
+      .get('/unknown?param=value')
+      .expect(404);
   });
 
   // TODO: remove comment after merge body validation
