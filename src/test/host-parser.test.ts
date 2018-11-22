e08e74aab1ef5a979f7acebc26d70eac39dedbfa
Mihaly Bezzeg <mihaly@brainsandbeards.com>
2018-11-22 09:30:16 +0100

    Remove subdomain parser for forwarded requests

--- src/test/host-parser.test.ts
+++ src/test/host-parser.test.ts
@@ -1,33 +1,33 @@
 import { parseHost } from '../helpers/host-parser';
 
 describe('Tests for Host Parser', () => {
-  it('Subdomain test', async () => {
-    const url = 'http://www.blog.classroom.me.uk/index.php';
-    const expected = 'classroom.me.uk';
+  it('Advanced url test', async () => {
+    const url = 'http://jsonplaceholder.typicode.com';
+    const expected = 'jsonplaceholder.typicode.com';
     const parsedHost = parseHost(url);
     expect(parsedHost).toEqual(expected);
   });
   it('Simple URL test', async () => {
     const url = 'http://www.youtube.com/watch?v=ClkQA2Lb_iE';
-    const expected = 'youtube.com';
+    const expected = 'www.youtube.com';
     const parsedHost = parseHost(url);
     expect(parsedHost).toEqual(expected);
   });
   it('HTTPS test', async () => {
     const url = 'https://www.youtube.com/watch?v=ClkQA2Lb_iE';
-    const expected = 'youtube.com';
+    const expected = 'www.youtube.com';
     const parsedHost = parseHost(url);
     expect(parsedHost).toEqual(expected);
   });
   it('WWW test', async () => {
     const url = "www.youtube.com/watch?v=ClkQA2Lb_iE";
-    const expected = 'youtube.com';
+    const expected = 'www.youtube.com';
     const parsedHost = parseHost(url);
     expect(parsedHost).toEqual(expected);
   });
   it('FTP test', async () => {
     const url = "ftps://ftp.websitename.com/dir/file.txt";
-    const expected = 'websitename.com';
+    const expected = 'ftp.websitename.com';
     const parsedHost = parseHost(url);
     expect(parsedHost).toEqual(expected);
   });
