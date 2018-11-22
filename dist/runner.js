64a28ae5b06d6ddaf7d77a9bf8fa4d2a496e25ca
Mihaly Bezzeg <mihaly@brainsandbeards.com>
2018-11-22 09:30:29 +0100

    Update dist

--- dist/runner.js
+++ dist/runner.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
-"use strict";var __importDefault=this&&this.__importDefault||function(e){return e&&e.__esModule?e:{default:e}};Object.defineProperty(exports,"__esModule",{value:!0});const commander_1=__importDefault(require("commander")),server_1=__importDefault(require("./server")),package_json_1=__importDefault(require("../package.json"));commander_1.default.version(package_json_1.default.version,"-v, --version").option("-c, --config <path>","Path to config file").parse(process.argv);const{config:config}=commander_1.default,server=new server_1.default(config);server.run();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const commander_1 = __importDefault(require("commander"));
+const server_1 = __importDefault(require("./server"));
+const package_json_1 = __importDefault(require("../package.json"));
+commander_1.default
+    .version(package_json_1.default.version, '-v, --version')
+    .option('-c, --config <path>', 'Path to config file')
+    .parse(process.argv);
+const { config } = commander_1.default;
+const server = new server_1.default(config);
+server.run();
+//# sourceMappingURL=runner.js.map
