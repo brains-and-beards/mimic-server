d6c456b8a0aa7bde8509ed5563fd17d86c679f6c
Mihaly Bezzeg <mihaly@brainsandbeards.com>
2019-03-29 11:43:33 +0100

    Cover additional test cases

--- src/test/importedConfigurations.test.ts
+++ src/test/importedConfigurations.test.ts
@@ -7,14 +7,29 @@ import {
 describe('Tests for importing configurations', () => {
   it('[GET] - Should return 200 imported endpoint', async () => {
     const res = await request(importedConfigPath)
-      .get('/test')
+      .post('/test')
+      .expect(200)
+      .expect('Content-Type', contentTypeText);
+    expect(res.text).toBe('test');
+  });
+  it('[GET] - Should return 200 when endpoint exist, but additional query params present', async () => {
+    const res = await request(importedConfigPath)
+      .post('/test?query=20')
+      .expect(200)
+      .expect('Content-Type', contentTypeText);
+    expect(res.text).toBe('test');
+  });
+  it('[GET] - Should return 200 when endpoint exist, but additional body present', async () => {
+    const res = await request(importedConfigPath)
+      .post('/test')
+      .send({ param: 'value' })
       .expect(200)
       .expect('Content-Type', contentTypeText);
     expect(res.text).toBe('test');
   });
   it('[GET] - Should return 404 for not existing imported project', async () => {
     const res = await request(importedConfigPath)
-      .get('/missing')
+      .post('/missing')
       .expect(404)
   });
   it('[GET] - Should return 404 for disabled imported project', async () => {
