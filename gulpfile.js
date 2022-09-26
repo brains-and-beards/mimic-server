492c576d97467212cace9537e30d3759e27fbfeb
Nacho López Sánchez <ncho.lopez@gmail.com>
2022-09-26 14:18:07 +0200

    use tsc instead of gulp (#10)
    * ignore dist
    
    * use tsc instead of gulp
    
    * use node14 version
    
    Co-authored-by: Nacho López <nacho.lopez@manomano.com>

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var uglify = require('gulp-uglify-es').default;
-
-let tsProject = ts.createProject('tsconfig.json');
-
-gulp.task('compile', function () {
-  return tsProject.src().pipe(tsProject()).pipe(uglify()).pipe(gulp.dest('dist/'));
-});
